c8115a15bf32c1008861f30d1aa6540ba0807071
patelsaheb1219 <kaumik1219@gmail.com>
2018-08-23 14:45:16 +0530

    Show json data instead of main file

--- app.js
+++ app.js
@@ -5,8 +5,39 @@ var path = require('path');
 
 var app = express();
 
+// var logger = function( req , res , next){
+//     console.log('Logging...');
+//     next();
+// }
+
+// app.use(logger);
+
+
+// Nody Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+//Set static path
+app.use(express.static(path.join(__dirname , 'public')));
+
+var person = [
+    {
+        name : 'Kishan Patel',
+        age : 21
+    },
+    {
+        name : 'Ravi Patel',
+        age : 21
+    },
+    {
+        name : 'Freddy Thobhani',
+        age : 21
+    }
+
+]
+
 app.get('/', function(req , res){
-    res.send('Hello World');
+    res.json(person);
 });
 
 app.listen(3000 , function(){

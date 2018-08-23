0757c7ab25031feb881dca137e8184406c81adba
patelsaheb1219 <kaumik1219@gmail.com>
2018-08-23 17:34:43 +0530

    Completer till form-validation

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-
+var expressValidator = require('express-validator');
 
 var app = express();
 
@@ -12,34 +12,86 @@ var app = express();
 
 // app.use(logger);
 
+//View Engine
+app.set('view engine' , 'ejs');
+app.set('views' , path.join(__dirname , 'views'));
 
-// Nody Parser Middleware
+// Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 
 //Set static path
 app.use(express.static(path.join(__dirname , 'public')));
 
-var person = [
-    {
-        name : 'Kishan Patel',
-        age : 21
-    },
-    {
-        name : 'Ravi Patel',
-        age : 21
-    },
-    {
-        name : 'Freddy Thobhani',
-        age : 21
+// Express Validator Middleware 
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+  
+      while(namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+        param : formParam,
+        msg   : msg,
+        value : value
+      };
     }
+  }));
 
+var users  = [ 
+    {
+        id : 1,
+        first_name : 'Kishan',
+        last_name : 'Patel',
+        email : 'kaumik@gmail.com'
+    },
+    {
+        id : 2,
+        first_name : 'Ravi',
+        last_name : 'Patel',
+        email : 'ravi@gmail.com'
+    },
+    {
+        id : 3,
+        first_name : 'Freddy',
+        last_name : 'Thobhani',
+        email : 'freddy@gmail.com'
+    }
 ]
 
 app.get('/', function(req , res){
-    res.json(person);
+    res.render('index' , {
+        title : 'Customers',
+        user : users
+    });
 });
 
+app.post('/user/add', function(req , res){
+
+    req.checkBody('firstName' , 'First Name is required').notEmpty();
+    req.checkBody('lastName' , 'last Name is required').notEmpty();
+    req.checkBody('email' , 'Email is required').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if(errors){
+        res.render('index' , {
+            title : 'Customers',
+            user : users,
+            errors:errors
+        });
+    } else {
+        var newUser = {
+            firstName : req.body.firstName,
+            lastName : req.body.lastName,
+            email : req.body.email
+        }
+        console.log('Success');
+    }
+})
 app.listen(3000 , function(){
     console.log('Server started on port 3000 ...')
 });
